d9502aa6acac956166d050060adb27880d8d0635
Lee Johnson <lee@givengain.ch>
2019-03-20 21:16:30 +0100

    change unfollow to nonFollowers=True
    rather than InstapyFollowed as the bot is following (or rather, not
    unfollowing) far too many users

--- bin/instagram_bot.py
+++ bin/instagram_bot.py
@@ -35,7 +35,7 @@ session.set_do_follow(
 # unfollow users who don't follow us
 session.unfollow_users(
     amount=1000,
-    InstapyFollowed=( True,"all" ),
+    nonFollowers=True,
     style="RANDOM",
     sleep_delay=60,
 )
